f696ab0175a23bc413cf07097d989bbfd121b7ff
xScottishKrissx <chrisdunne66@gmail.com>
2018-03-23 12:35:59 +0000

    Added Click to View and Expand Captions, Added Scroll to Top Sitewide and added title for gallery area

--- assets/js.js
+++ assets/js.js
@@ -2,13 +2,47 @@ $(document).ready(function() {
   console.log("jquery is working");
 
   //scroll to section process page
-  function scrollToAnchor(aid){
-      var aTag = $("div[name='"+ aid +"']");
-      $('html,body').animate({scrollTop: aTag.offset().top},5000);
+  function scrollToAnchor(aid) {
+    var aTag = $("div[name='" + aid + "']");
+    $('html,body').animate({
+      scrollTop: aTag.offset().top
+    }, 300);
   }
 
-  $(".scrollTop").click(function() {
-     scrollToAnchor('top');
+  $(".scroll-to-top-btn").click(function() {
+    scrollToAnchor('top');
   });
+
+
+  //Click to View Caption
+  //$(".click-caption").click(function(){
+  //$(".click-caption").css("top","0%");
+  //$(".click-caption").addClass("show");
+
+  //});
+  $(".click-caption").click(function() {
+    $(this).toggleClass("show")
+  });
+
+  $(".expand-caption").on("click",function() {
+    //$("col-12 col-md-4 .box-3").addClass("col-1")
+    $(this).parent().toggleClass("expand-image");
+  });
+
+  $(window).on("scroll",function() {
+    //$("col-12 col-md-4 .box-3").addClass("col-1")
+    if ($(window).scrollTop() < 1000) {
+      $(".scroll-to-top-btn").css("visibility","hidden");
+
+    }else if($(window).scrollTop() > 1000){
+        $(".scroll-to-top-btn").css("visibility","visible");
+    };
+  });
+
+  //$(".hide-caption").click(function(){
+  //  $(".click-caption").css("top","90%");
+  //});
+
+
 
 });
